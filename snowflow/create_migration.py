ff0423817c87905d01c3a4d0a4ee95f2fcfb3b01
Rodion G <rodion.g.work@gmail.com>
2023-04-14 17:25:46 +0300

    added generating new migration and opening pr

--- snowflow/create_migration.py
+++ snowflow/create_migration.py
@@ -1,7 +1,12 @@
+import random
+import string
 
 
 def main():
-    print("Running main")
+    letters = string.ascii_lowercase
+    uniq_name = ''.join(random.choice(letters) for i in range(20))
+    with open(f'migrations/{uniq_name}.sql', 'w')as f:
+        f.write('select * from user;')
 
 
 if __name__ == '__main__':
